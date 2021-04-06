88d957662ee62a73449259cd9d0e5a5ff09e0509
Amro dandashli <amrdandashli@gmail.com>
2021-04-06 18:40:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 
  var num2= 31 ;
  var num1= 2 ;
- Math.random = 0.4433
+ var num3 = 21;
+ var num4 = 17;
   var multiply = num1 * num2;
-  var random  = Math.random() ;
+  var random  = Math.floor() ;
   var mod = num3 % num4;
   var max  = Math.max(1,2,3,4,5,20)
